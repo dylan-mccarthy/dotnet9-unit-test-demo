cbe17d1fb75ed49643535495a3addb5d52b21bf0
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2024-11-18 10:10:08 +0000

    initial demo commit

---
+++ TestLib.Tests/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using TestLib;
+
+namespace TestLib.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var demo = new Demo();
+            Assert.Equal("Hello, World!", demo.SayHello("World"));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var demo = new Demo();
+            Assert.Equal("Goodbye, World!", demo.SayGoodbye("World"));
+        }
+
+        [Fact]
+        public void ShortRunningMethodTest()
+        {
+            // Given
+            var demo = new Demo();
+        
+            // When
+            demo.ShortRunningMethod();
+        
+            // Then
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void MediumRunningMethodTest()
+        {
+            // Given
+            var demo = new Demo();
+        
+            // When
+            demo.MediumRunningMethod();
+        
+            // Then
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void LongRunningMethodTest()
+        {
+            // Given
+            var demo = new Demo();
+        
+            // When
+            demo.LongRunningMethod();
+        
+            // Then
+            Assert.True(true);
+        }
+
+    }
+}
---
+++ TestLib/Demo.cs
@@ -0,0 +1,40 @@
+﻿namespace TestLib
+{
+    public class Demo
+    {
+        public string SayHello(string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+        public string SayGoodbye(string name)
+        {
+            return $"Goodbye, {name}!";
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public void ShortRunningMethod()
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        public void MediumRunningMethod()
+        {
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        public void LongRunningMethod()
+        {
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
